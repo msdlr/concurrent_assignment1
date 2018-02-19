b9a775a9ae0f36681a30774127a8527422214903
usuario <usuario@161.67.196.149>
2018-02-19 10:05:09 +0100

    part c done

--- src/A/Reference.java
+++ src/A/Reference.java
@@ -5,4 +5,29 @@ package A;
 
 public class Reference {
 
+   public void main() throws InterruptedException{
+       Thread main = Thread.currentThread();
+     
+       
+       System.out.println(main.getName());
+       
+      for (int i = 0; i <=100; i++) {
+                if(i%2==1){
+               System.out.println(i);
+            main.setName(String.valueOf(i));
+            Thread.sleep(2*1000);
+           
+                }
+       }
+       
+   } 
+  
+    
+        
+   
+    
+
+    
+        
+   
 }

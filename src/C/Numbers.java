40b8ff6025584ca63bf52771bdac419590997355
usuario <usuario@161.67.196.149>
2018-02-26 09:10:39 +0100

    part c done

--- src/C/Numbers.java
+++ src/C/Numbers.java
@@ -9,5 +9,21 @@ package C;
  */
 
 public class Numbers {
-
+ public Numbers() {
+    
+    }
+   
+    public void main() throws InterruptedException {
+    R_PrintID idPrinter1 = new R_PrintID();    
+    R_PrintID idPrinter2 = new R_PrintID();    
+    R_PrintID idPrinter3 = new R_PrintID();    
+    R_PrintID idPrinter4 = new R_PrintID();    
+    R_PrintID idPrinter5 = new R_PrintID();
+    
+    idPrinter1.start();
+    idPrinter2.start();
+    idPrinter3.start();
+    idPrinter4.start();
+    idPrinter5.start();
+    }
 }
--- src/C/R_PrintID.java
+++ src/C/R_PrintID.java
@@ -1,4 +1,9 @@
 package C;
+
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Modify class concurrent_assignment1.B.TPrintID so that it is created implementing Runnable 
  * (call the new class R_PrintID). 
@@ -8,6 +13,30 @@ package C;
  *
  */
 
-public class R_PrintID {
+public class R_PrintID  extends Thread {
+long variableID;
+        Random r = new Random();
+/* Constructor */
+    public R_PrintID() {
+        this.variableID = this.getId();
+    }
+
+/* Methods */    
+
+ 
+    @Override
+    public void run()  {
+    for (int i = 0; i < 10; i++) {
+
+        System.out.println("ID: "+this.variableID);
+
+        try {    
+            Thread.sleep(r.nextInt(1000));
+        } catch (InterruptedException ex) {
+            Logger.getLogger(R_PrintID.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
 
 }
